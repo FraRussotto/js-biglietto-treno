f3c2b3c1a7d4bb8e201ad177125394bd1f3ed0c6
FraRussotto <russottofrancesco1@gmail.com>
2023-09-07 16:48:41 +0200

    [ADD] Output

--- js/script.js
+++ js/script.js
@@ -2,18 +2,25 @@
   Dichiaro la variabile col prezzo al km del viaggio
   Creo l'IF per stabilire in quale range di età rientra l'utente*/
 
-
+const outputTag = document.getElementById('output');
 const distance = prompt('Inserisci i km da percorrere');
 const age = prompt('Inserisci la tua età');
 const km_price = 0.21;
 const discount20 = 20;
 const discount40 = 40;
 let final_price = distance * km_price;
+let price_discount;
 
 if(age < 18){
   console.log(final_price = (final_price - ((final_price * discount20) / 100)));
-}
-else if(age > 65){
+  price_discount = final_price.toFixed(2);
+}else if(age > 65){
   console.log(final_price = (final_price - ((final_price * discount40) / 100)));
+  price_discount = final_price.toFixed(2);
 }
 console.log(final_price);
+price_discount = final_price.toFixed(2);
+
+const message = 'Il costo del biglietto è' + ' ' + price_discount + '€';
+
+outputTag.innerHTML = message;
