cc9ef8dd47eb3161789f0e217473ba598546db31
FraRussotto <russottofrancesco1@gmail.com>
2023-09-08 16:10:31 +0200

    [FIX] Controllo inserimento dati

--- js/script.js
+++ js/script.js
@@ -1,26 +1,46 @@
-/* Savlo i prompt dell'utente (età e km),
+/* Salvo i prompt dell'utente (età e km),
   Dichiaro la variabile col prezzo al km del viaggio
   Creo l'IF per stabilire in quale range di età rientra l'utente*/
 
-const outputTag = document.getElementById('output');
-const distance = prompt('Inserisci i km da percorrere');
-const age = prompt('Inserisci la tua età');
-const km_price = 0.21;
-const discount20 = 20;
-const discount40 = 40;
-let final_price = distance * km_price;
-let price_discount;
+  const km_price = 0.21;
+  const discount20 = 20;
+  const discount40 = 40;
 
-if(age < 18){
-  console.log(final_price = (final_price - ((final_price * discount20) / 100)));
-  price_discount = final_price.toFixed(2);
-}else if(age > 65){
-  console.log(final_price = (final_price - ((final_price * discount40) / 100)));
-  price_discount = final_price.toFixed(2);
-}
-console.log(final_price);
-price_discount = final_price.toFixed(2);
+  let insertOk = true;
+  let age;
+  let message;
+  let discountMessage = ``;
 
-const message = 'Il costo del biglietto è' + ' ' + price_discount + '€';
+  const distance = parseInt(prompt('Inserisci la distanza da percorrere'))
 
-outputTag.innerHTML = message;
+  if(isNaN(distance)){
+    insertOk = false;
+    message = 'Il dato sulla distanza non è corretto';
+  }else{
+    age = parseInt(prompt(`Inserisci la tua età`));
+    
+    if(isNaN(age)){
+    insertOk = false;
+    message = 'Il dato sull\'età non è corretto';
+  }
+  }
+  if(insertOk){
+
+    const price = distance * km_price;
+    let final_price = price;
+
+    if(age < 18){
+      final_price *= 1 - (discount20 / 100);
+      discountMessage = `<h2>Essendo minorenne hai uno sconto del 20&percnt; quindi pagherai ${final_price} anzichè ${price}</h2>`;
+
+    }else if(age >= 65){
+      final_price *= 1 - (discount40 / 100); 
+      discountMessage = `<h2>Essendo over 65 hai uno sconto del 40&percnt; quindi pagherai ${final_price} anzichè ${price}</h2>`;
+    }    
+    message = 'Il costo del biglietto è' + ' ' + price + '€';
+  }
+  
+  
+  document.getElementById('output').innerHTML = `
+  ${message}
+  ${discountMessage}`;
